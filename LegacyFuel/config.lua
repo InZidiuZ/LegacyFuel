2178ba120e8499f43482338d3488d37ba840bca1
gregory propst <gregory.propst@gmail.com>
2019-02-13 01:35:16 -0500

    add config option to disable HUD

--- LegacyFuel/config.lua
+++ LegacyFuel/config.lua
@@ -4,3 +4,4 @@ Config.EnableBlips				= true
 Config.EnableJerryCans			= true
 Config.EnableBuyableJerryCans	= true -- Coming soon, currently useless
 Config.VehicleFailure			= 10 -- At what fuel-percentage should the engine stop functioning properly? (Defualt: 10)
+Config.ShouldDisplayHud         = true
