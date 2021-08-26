18ee00d97720e18600bc310eb0a3e2da67294688
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-27 01:54:48 +0300

    very big blip

--- functions/functions_client.lua
+++ functions/functions_client.lua
@@ -44,7 +44,7 @@ function CreateBlip(coords)
 	local blip = AddBlipForCoord(coords)
 
 	SetBlipSprite(blip, 361)
-	SetBlipScale(blip, 0.9)
+	SetBlipScale(blip, 0.6)
 	SetBlipColour(blip, 4)
 	SetBlipDisplay(blip, 4)
 	SetBlipAsShortRange(blip, true)
